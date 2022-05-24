f27dd45c1fa7abe326932b319e0f09485619bd52
Emir Jasarevic <emir2000@hotmail.se>
2022-05-24 20:25:44 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -12,3 +12,12 @@ acc[i].addEventListener("click", function() {
     }
 });
 }
+
+let vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
